0632fbe4126923014c5a1a7421821d5310dae77e
Anatoly <90443893+Carineitor77@users.noreply.github.com>
2023-01-13 11:13:51 +0200

    Added a CRUD features using the CQRS + Mediator pattern

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,6 @@
+using API.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -10,40 +10,23 @@ using System;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("CorsPolicy", policy =>
-    {
-        policy.AllowAnyMethod()
-              .AllowAnyHeader()
-              .WithOrigins("http://localhost:3000");
-    });
-});
-
-builder.Services.AddDbContext<DataContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
-	try
-	{
+    try
+    {
         var context = scope.ServiceProvider.GetRequiredService<DataContext>();
         await context.Database.MigrateAsync();
         await Seed.SeedData(context);
     }
-	catch (Exception ex)
-	{
+    catch (Exception ex)
+    {
         var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
         logger.LogError(ex, "An error occured during migration");
-	}
+    }
 }
 
 if (app.Environment.IsDevelopment())
